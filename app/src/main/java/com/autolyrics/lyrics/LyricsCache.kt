65d91eaa056d421ba472f1a12dc32f008e1acd08
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-12 21:21:41 +0900

    Invalidate cache for PetitLyrics diagnostics

--- app/src/main/java/com/autolyrics/lyrics/LyricsCache.kt
+++ app/src/main/java/com/autolyrics/lyrics/LyricsCache.kt
@@ -109,7 +109,7 @@ class LyricsCache(context: Context) {
         }
 
         val key = listOf(
-            "v5",
+            "v6",
             normalizeKeyPart(track.title),
             normalizeKeyPart(track.artist),
             normalizeKeyPart(track.album),
