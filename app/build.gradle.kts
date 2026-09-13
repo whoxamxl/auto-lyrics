34e044a95d8398d5cc33e90ce20ba8a7e40e7a25
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-13 16:16:55 +0900

    Bump version to 1.9.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.autolyrics"
         minSdk = 26
         targetSdk = 34
-        versionCode = 31
-        versionName = "1.9.7"
+        versionCode = 32
+        versionName = "1.9.8"
 
         buildConfigField(
             "String",
