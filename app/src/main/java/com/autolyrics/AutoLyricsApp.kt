71ed2306a91b20d7f75aa0ea82d4863a38250963
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-14 08:05:05 +0900

    Install translation status UI in phone settings

--- app/src/main/java/com/autolyrics/AutoLyricsApp.kt
+++ app/src/main/java/com/autolyrics/AutoLyricsApp.kt
@@ -1,7 +1,10 @@
 package com.autolyrics
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.autolyrics.auto.LyricsBrowserService
 import com.autolyrics.media.MediaTracker
 
@@ -9,6 +12,25 @@ class AutoLyricsApp : Application() {
     override fun onCreate() {
         super.onCreate()
         MediaTracker.init(this)
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+
+            override fun onActivityStarted(activity: Activity) = Unit
+
+            override fun onActivityResumed(activity: Activity) {
+                if (activity is AppCompatActivity) {
+                    TranslationStatusView.install(activity)
+                }
+            }
+
+            override fun onActivityPaused(activity: Activity) = Unit
+
+            override fun onActivityStopped(activity: Activity) = Unit
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+
+            override fun onActivityDestroyed(activity: Activity) = Unit
+        })
         try {
             startService(Intent(this, LyricsBrowserService::class.java))
         } catch (_: Exception) { }
