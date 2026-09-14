d8f34e4cfd270a45e1cfae6f65f7f926d95d3e8a
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-14 17:48:58 +0900

    Release v1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.autolyrics"
         minSdk = 26
         targetSdk = 34
-        versionCode = 36
-        versionName = "1.11.0"
+        versionCode = 37
+        versionName = "1.12.0"
 
         buildConfigField(
             "String",
